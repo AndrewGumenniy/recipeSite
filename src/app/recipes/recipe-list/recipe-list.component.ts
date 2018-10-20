e4e318271fcba88b7b736bae3282956b59563583
Andrew Gumenniy <Gumenniy.oik@gmail.com>
2018-10-20 23:11:00 +0300

    services and dropdown directive

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,20 +9,11 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() selectedRecipe = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe('Super Burger', 'Burger with double meat', 'https://www.mcdonalds.gr/sites/default/files/produits/big-mac.png'),
-    new Recipe('Tasty chicken', 'Chicken with apples and rice',
-    'http://www.frekuenca.com/wp-content/uploads/2017/12/Turkey-with-Cornbread-Stuffing-iStock.jpg')
-  ];
-
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-  }
-
-  onSelect(recipe: Recipe) {
-    this.selectedRecipe.emit(recipe);
+    this.recipes = this.recipeService.getRecipes();
   }
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,35 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+    constructor(private slService: ShoppingListService) {}
+    recipeSelected = new EventEmitter<Recipe>();
+    recipes: Recipe[] = [
+        new Recipe('Super Burger',
+        'Burger with double meat',
+        'https://www.mcdonalds.gr/sites/default/files/produits/big-mac.png',
+        [
+            new Ingredient('meat', 1),
+            new Ingredient('tomato', 2)
+        ]),
+        new Recipe('Tasty chicken',
+        'Chicken with apples and rice',
+        'http://www.frekuenca.com/wp-content/uploads/2017/12/Turkey-with-Cornbread-Stuffing-iStock.jpg',
+        [
+            new Ingredient('chicken', 1),
+            new Ingredient('rice', 2)
+        ])
+    ];
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+    }
+}
